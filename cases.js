ba39919213a2530f0c3d02370aa222cd20454942
zhangzheng <zhangzheng@qianbao.com>
2017-01-18 17:59:56 +0800

    增加post方法的支持

--- cases.js
+++ cases.js
@@ -2,10 +2,25 @@
 cases = [
   {
     "desc": "resthub unit-test 1",
+    "method": "GET",
+    "params": "",
+    "data": {},
+    "jsonBool": true,
     "url": "http://apis.qianbao.com/basicservice/v1/gateway_unit_test",
     "httpStatus": 200,
     "jsonStatus": "20000000",
   },
+  {
+    "desc": "resthub unit-test 1",
+    "method": "GET",
+    "params": "",
+    "data": {},
+    "jsonBool": true,
+    "url": "http://apis.qianbao.com/basicservice/v1/gateway_unit_test",
+    "httpStatus": 200,
+    "jsonStatus": "20000000",
+  },
+
 ]
 
 module.exports = cases;
--- darts_spec.js
+++ darts_spec.js
@@ -3,12 +3,28 @@ var cases = require('./cases');
 
 for (var i = 0;i < cases.length; i++) {
   var item = cases[i];
-  frisby.create(item.desc)
-    .get(item.url)
-      .addHeader('Uni-Source', 'resthub/ci')
-      .expectStatus(item.httpStatus)
-      .expectJSON({
-          status: jsonStatus
-      }
-  .toss();
+
+  //GET
+  if (item.method.toUpperCase() === "GET") {
+    frisby.create(item.desc)
+      .get(item.url + "?" + item.params)
+        .addHeader('Uni-Source', 'resthub/ci')
+        .expectStatus(item.httpStatus)
+        .expectJSON({
+          status: item.jsonStatus
+        })
+    .toss();
+  }
+
+  //POST
+  if (item.method.toUpperCase() === "POST") {
+    frisby.create(item.desc)
+      .post(item.url, item.data, {json: item.jsonBool})
+        .addHeader('Uni-Source', 'resthub/ci')
+        .expectStatus(item.httpStatus)
+        .expectJSON({
+          status: item.jsonStatus
+        })
+    .toss();
+  }
 }
