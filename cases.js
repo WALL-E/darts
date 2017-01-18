bfb0b04d4e89bd9243dc2efbd1344beb27574f19
zhangzheng <zhangzheng@qianbao.com>
2017-01-18 19:03:32 +0800

    完成测试用例

--- cases.js
+++ cases.js
@@ -11,14 +11,16 @@ cases = [
     "jsonStatus": "20000000",
   },
   {
-    "desc": "resthub unit-test 1",
-    "method": "GET",
+    "desc": "resthub unit-test 2",
+    "method": "POST",
     "params": "",
-    "data": {},
+    "data": {
+      "name":"darts",
+    },
     "jsonBool": true,
     "url": "http://apis.qianbao.com/basicservice/v1/gateway_unit_test",
     "httpStatus": 200,
-    "jsonStatus": "20000000",
+    "jsonStatus": 401,
   },
 
 ]
