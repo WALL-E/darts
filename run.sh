a79a705d60f7b9b1eb7455e09bf77143d47df69e
zhangzheng <zhangzheng@qianbao.com>
2017-01-18 18:56:55 +0800

    增加检查是否安装jasmine-node

--- run.sh
+++ run.sh
@@ -2,4 +2,12 @@
 
 cd `dirname $0`
 
+command jasmine-node --version 1>/dev/null 2>/dev/null
+ret=$?
+if test $ret -ne 0
+then
+    echo "需要安装jasmine-node"
+    exit 1
+fi
+
 jasmine-node darts_spec.js --junitreport
